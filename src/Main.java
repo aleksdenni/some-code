674173b95a10819bd3a8610a087f7aed345687cf
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-11-16 20:40:42 +0200

    Add files via upload
    this code counts the number of occurrences of a substring in the string

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        String sub = scanner.nextLine();
+        Pattern p = Pattern.compile(sub);
+        Matcher m = p.matcher(str);
+        int counter = 0;
+        while (m.find()) {
+            counter++;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int lastIndex = sub.length();
+        for (int i = 0; i < counter; i++) {
+            if (i == 0) {
+                result.append(str.indexOf(sub) + 1);
+            } else {
+                result.append(" ").append(str.indexOf(sub, lastIndex) + 1);
+                lastIndex = str.indexOf(sub, lastIndex) + 1;
+            }
+        }
+
+        System.out.println(counter);
+        System.out.println(result);
+    }
+}
+//abracadabra
