162b717347e021f2c3c30c1864d3b960f4fac43d
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-12-18 00:29:23 +0200

    Update class

--- src/linked/LinkList.java
+++ src/linked/LinkList.java
@@ -1,19 +1,41 @@
 package linked;
 
 public class LinkList {
-    private final Node first = new Node();
-    private final Node last = new Node();
+    private Node first = new Node();
+    private Node last = new Node();
 
-    public LinkList() {
-        first.next = last;
-        //last.prev = first;
+    public void add(String value) {
+        if (first.next == null) {
+            Node node = new Node();
+            node.value = value;
+            first.next = node;
+        }
+        if (last.prev == null) {
+            last.prev = first.next;
+            return;
+        }
+
+        Node node = new Node();
+        node.value = value;
+
+        Node lastNode = last.prev;
+        lastNode.next = node;
+        node.prev = lastNode;
+        last.prev = node;
     }
 
-    public void add(String str){
-
+    public String get(int index) {
+        if(index<0) return null;
+        Node tempVar = first.next;
+        for (int i = 0; i < index ; i++) {
+            tempVar = tempVar.next;
+            if (tempVar==null) return null;
+        }
+        return tempVar.value;
     }
+
     public static class Node {
-        //private Node prev;
+        private Node prev;
         private String value;
         private Node next;
     }
