d87c04d65cd965cfcb52a80e8221fc0109467e58
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-12-18 00:04:38 +0200

    made stack

---
+++ src/linked/MyStack.java
@@ -0,0 +1,29 @@
+package linked;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyStack {
+
+    private final List<String> storage = new LinkedList<>();
+
+    public void push(String s) {
+        storage.add(0 , s);
+    }
+
+    public String pop() {
+        return storage.remove(0);
+    }
+
+    public String peek() {
+        return storage.get(0);
+    }
+
+    public boolean empty() {
+        return storage.isEmpty();
+    }
+
+    public int search(String s) {
+        return storage.indexOf(s);
+    }
+}
