ecb4a80343866e0b43ef319575e81f66c0084e95
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2022-12-17 23:08:33 +0200

    add regex for change collect to arr

--- src/regex/TestRegex.java
+++ src/regex/TestRegex.java
@@ -6,12 +6,13 @@ public class TestRegex {
     public static void main(String[] args) {
         String str = "My Phone number is out of range. This is Mark";
         str = str.replaceAll("^(\\w+)(.*)([ ]+)(\\w+)$", "$4$3$2$3$1");
-        System.out.print(str);
+        System.out.println(str);
 
-        List<Object> set = new ArrayList<>(Arrays.asList("hello",1,1,2,3));
+        List<Object> set = new ArrayList<>(Arrays.asList("hello", 1, 1, 2, 3));
+        Collections.addAll(set, 1, 2, 3, 4, "22", 1, "2", "1");
         Set<Object> st = new HashSet<>();
         Collections.addAll(st, set.toString()
-                .replaceAll("[\\[\\]]","")
+                .replaceAll("[\\[\\]]", "")
                 .split(", "));
         st.forEach(System.out::println);
     }
