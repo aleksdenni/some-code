e8893c9766ea111ef64a85fb7c4fc683a1edb050
Aleksey Chekaliuk <47951470+aleksdenni@users.noreply.github.com>
2023-12-04 10:21:42 +0200

    перевантаження методів

---
+++ src/methods/Overload.java
@@ -0,0 +1,19 @@
+package methods;
+
+public class Overload {
+
+    public void methodOne(){
+    }
+
+    public int methodOne(int i){
+        return i;
+    }
+
+    private void methodOne(String str){
+
+    }
+
+    public static void methodOne(byte i){
+    }
+
+}
